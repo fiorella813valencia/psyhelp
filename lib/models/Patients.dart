e06f232599489d5893bd13f8160dc31bbe509b4b
fiorella813valencia <89022353+fiorella813valencia@users.noreply.github.com>
2023-06-30 21:50:28 -0500

    Initial commit

---
+++ lib/models/Patients.dart
@@ -0,0 +1,15 @@
+class Patient{
+  final String name;
+  final String lastName;
+  final String age;
+  final String illness;
+  final String allergies;
+
+  const Patient({
+    required this.name,
+    required this.lastName,
+    required this.age,
+    required this.illness,
+    required this.allergies
+});
+}
